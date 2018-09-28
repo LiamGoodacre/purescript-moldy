c0f3a19b6b3d3107433d38d5606f2795735b4652
Tomasz Rybarczyk <paluho@gmail.com>
2018-09-29 00:01:51 +0200

    Updates for 0.12

--- test/Main.purs
+++ test/Main.purs
@@ -1,21 +1,21 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+
 import Data.Foldable (foldMap)
 import Data.Moldy (moldMap, moldy)
-import Data.Char (toUpper)
-import Data.String (singleton)
+import Data.String (singleton, toUpper)
+import Effect (Effect)
+import Effect.Console (log)
 
 foo :: String
 foo = "Foo"
 
 foo' :: String
-foo' = moldMap (singleton <<< toUpper) foo
+foo' = moldMap (toUpper <<< singleton) foo
 
 foo'' :: String
-foo'' = foldMap (singleton <<< toUpper) (moldy foo)
+foo'' = foldMap (toUpper <<< singleton) (moldy foo)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: Effect Unit
 main = when (foo' == foo'') $ log "Done!"
